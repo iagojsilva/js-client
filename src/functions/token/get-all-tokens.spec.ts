f025c83724fe91f48ec40b4c1948fd238ccd3f21
iago.silva <iago.silva@lucaspaganini.com>
2021-11-17 15:18:50 -0300

    getAllTokens function test

--- src/functions/token/get-all-tokens.spec.ts
+++ src/functions/token/get-all-tokens.spec.ts
@@ -5,3 +5,54 @@
  * This software may be modified and distributed under the terms of the
  * MIT license. See the LICENSE file for details.
  **************************************************************************/
+
+import { TokenCreationRequest, isToken } from '~/models';
+import { integrationTest, TEST_BASE_API_CONTEXT } from '~/tests';
+import { makeCreateOneToken } from './create-one-token';
+import { makeDeleteOneToken } from './delete-one-token';
+import { makeGetAllTokens } from './get-all-tokens';
+
+describe('getAllTokens()', () => {
+	const createOneToken = makeCreateOneToken(TEST_BASE_API_CONTEXT);
+	const deleteOneToken = makeDeleteOneToken(TEST_BASE_API_CONTEXT);
+	const getAllTokens = makeGetAllTokens(TEST_BASE_API_CONTEXT);
+
+	beforeEach(async () => {
+		// Delete all tokens
+		const currentTokens = await getAllTokens();
+		const currentTokenIDs = currentTokens.map(m => m.id);
+		const deletePromises = currentTokenIDs.map(TokenID => deleteOneToken(TokenID));
+		await Promise.all(deletePromises);
+	});
+
+	it(
+		'Should return all tokens',
+		integrationTest(async () => {
+			// Create two tokens
+			const creatableTokens: Array<TokenCreationRequest> = [
+				{
+					name: 'Q1',
+					capabilities: ['KitWrite'],
+				},
+				{
+					name: 'Q2',
+					capabilities: ['KitWrite'],
+				},
+			];
+			const createPromises = creatableTokens.map(creatable => createOneToken(creatable));
+			await Promise.all(createPromises);
+
+			const tokens = await getAllTokens();
+			expect(tokens.length).toBe(2);
+			expect(tokens.every(isToken)).toBeTrue();
+		}),
+	);
+
+	it(
+		'Should return an empty array if there are no tokens',
+		integrationTest(async () => {
+			const tokens = await getAllTokens();
+			expect(tokens.length).toBe(0);
+		}),
+	);
+});
