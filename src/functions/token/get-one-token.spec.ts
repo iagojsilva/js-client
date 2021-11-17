e439cdf796fadcce8fc772e4e3354b2c07a52a18
iago.silva <iago.silva@lucaspaganini.com>
2021-11-17 15:02:20 -0300

    getOneToken function test

--- src/functions/token/get-one-token.spec.ts
+++ src/functions/token/get-one-token.spec.ts
@@ -5,3 +5,51 @@
  * This software may be modified and distributed under the terms of the
  * MIT license. See the LICENSE file for details.
  **************************************************************************/
+
+import { isToken, TokenCreatedResponse, TokenCreationRequest } from '~/models';
+import { integrationTest, TEST_BASE_API_CONTEXT } from '~/tests';
+import { makeCreateOneToken } from './create-one-token';
+import { makeDeleteOneToken } from './delete-one-token';
+import { makeGetAllTokens } from './get-all-tokens';
+import { makeGetOneToken } from './get-one-token';
+
+describe('getOneToken()', () => {
+	const getOneToken = makeGetOneToken(TEST_BASE_API_CONTEXT);
+	const createOneToken = makeCreateOneToken(TEST_BASE_API_CONTEXT);
+	const getAllTokens = makeGetAllTokens(TEST_BASE_API_CONTEXT);
+	const deleteOneToken = makeDeleteOneToken(TEST_BASE_API_CONTEXT);
+
+	let createdToken: TokenCreatedResponse;
+
+	beforeEach(async () => {
+		// Delete all tokens
+		const currentTokens = await getAllTokens();
+		const currentTokensIDs = currentTokens.map(t => t.id);
+		const deletePromises = currentTokensIDs.map(tokenID => deleteOneToken(tokenID));
+		await Promise.all(deletePromises);
+
+		// Create a Token
+		const data: TokenCreationRequest = {
+			name: 'T1',
+			descriptions: 'My T1',
+			capabilities: ['AttachSearch', 'KitWrite'],
+		};
+		createdToken = await createOneToken(data);
+	});
+
+	it(
+		'Returns a token',
+		integrationTest(async () => {
+			const token = await getOneToken(createdToken.id);
+			expect(isToken(token)).toBeTrue();
+			expect(token).toEqual(createdToken);
+		}),
+	);
+
+	it(
+		"Returns an error if the token doesn't exist",
+		integrationTest(async () => {
+			await expectAsync(getOneToken('non-existent')).toBeRejected();
+		}),
+	);
+});
