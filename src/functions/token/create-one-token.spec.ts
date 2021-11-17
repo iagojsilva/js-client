09602213db23a61c3929dd59f2ccd0a4d2428304
iago.silva <iago.silva@lucaspaganini.com>
2021-11-17 15:10:04 -0300

    createOneToken function test

--- src/functions/token/create-one-token.spec.ts
+++ src/functions/token/create-one-token.spec.ts
@@ -5,3 +5,30 @@
  * This software may be modified and distributed under the terms of the
  * MIT license. See the LICENSE file for details.
  **************************************************************************/
+
+import { isToken, TokenCreationRequest } from '~/models';
+import { integrationTest, myCustomMatchers, TEST_BASE_API_CONTEXT } from '~/tests';
+import { makeCreateOneToken } from './create-one-token';
+
+describe('createOneToken()', () => {
+	const createOneToken = makeCreateOneToken(TEST_BASE_API_CONTEXT);
+
+	beforeEach(async () => {
+		jasmine.addMatchers(myCustomMatchers);
+	});
+
+	it(
+		'Should create a token and return it',
+		integrationTest(async () => {
+			const data: TokenCreationRequest = {
+				name: 'name',
+				description: 'description',
+				capabilities: ['KitWrite'],
+			};
+
+			const token = await createOneToken(data);
+			expect(isToken(token)).toBeTrue();
+			expect(token).toPartiallyEqual(data);
+		}),
+	);
+});
